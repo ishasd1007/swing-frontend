039d23e43d91572fd2a620403002b38e65c50136
ishasd1007 <ishag8064@gmail.com>
2026-03-25 22:59:21 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://swing-backend-eejk.onrender.com/api",
-  headers: {
-    "Content-Type": "application/json"
-  }
+  baseURL: "https://swing-backend-1.onrender.com/api"
 });
 
 export const loginUser = (data) => API.post("/auth/login", data);
-
 export const registerUser = (data) => API.post("/auth/register", data);
