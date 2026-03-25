b2ae6836ed7b5e1d7eb87c45712bd752969fd70f
ishasd1007 <ishag8064@gmail.com>
2026-03-25 23:03:58 +0530

    Switch to HashRouter and update route paths
    Replaced BrowserRouter with HashRouter and updated routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Home from "./pages/Home";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
