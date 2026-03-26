129919f07c8ef55ea511609ec748f3aaa703b9c0
ishasd1007 <ishag8064@gmail.com>
2026-03-26 09:46:53 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://swing-backend-1.onrender.com/api",
+  baseURL: "https://swing-backend-2.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
