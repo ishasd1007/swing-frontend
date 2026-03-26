1d23f30baf90306137c8fe657c41ff741b8e5ea4
ishasd1007 <ishag8064@gmail.com>
2026-03-26 09:32:23 +0530

    Change API base URL to new backend
    Updated API base URL to point to the new backend.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://swing-backend-1.onrender.com/api"
+  baseURL: "https://swing-backend-2.onrender.com/api"
 });
 
 export const loginUser = (data) => API.post("/auth/login", data);
